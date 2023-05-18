eb8414cfb59bd7b58a524b3ced57c30a688aba98
sunshouhua <65739056@qq.com>
2023-05-18 16:14:50 +0800

    done

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='filename-timestamp',  # 您的库的名称
     version='1.0.0',  # 版本号
     author='Sun',  # 作者名称
-    author_email='xxx@example.com',  # 作者邮箱
+    # author_email='xxx@example.com',  # 作者邮箱
     description='timestamp for filename',  # 描述
     packages=[],  # 包含的Python包
     py_modules=['filename_timestamp'],  # 主文件名称
